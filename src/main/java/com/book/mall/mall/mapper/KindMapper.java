b3b44b93465fceb22aacb431c410dda79ef1841c
jingrui <jingrui@36kr.com>
2019-04-10 20:24:49 +0800

    kind列表接口未分页

--- src/main/java/com/book/mall/mall/mapper/KindMapper.java
+++ src/main/java/com/book/mall/mall/mapper/KindMapper.java
@@ -1,6 +1,10 @@
 package com.book.mall.mall.mapper;
 
 import com.book.mall.mall.entity.Kind;
+import com.book.mall.mall.reqform.KindAddReqForm;
+import com.book.mall.mall.resbean.KindAddResBean;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Options;
 import org.apache.ibatis.annotations.Select;
 import org.springframework.stereotype.Component;
 
@@ -11,4 +15,8 @@ public interface KindMapper {
 
     @Select("select * from kind")
     public List<Kind> findAll();
+
+    @Options(useGeneratedKeys = true, keyProperty = "id")
+    @Insert("insert into kinds (name, image, createTime) values (#{name}, #{image}, #{createTime)")
+    public KindAddResBean add(KindAddReqForm reqForm);
 }
---
+++ src/main/java/com/book/mall/mall/reqform/KindAddReqForm.java
@@ -0,0 +1,41 @@
+package com.book.mall.mall.reqform;
+
+public class KindAddReqForm {
+
+    private Long id;
+    private String name;
+    private String iamge;
+    private Long createTime;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIamge() {
+        return iamge;
+    }
+
+    public void setIamge(String iamge) {
+        this.iamge = iamge;
+    }
+
+    public Long getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Long createTime) {
+        this.createTime = createTime;
+    }
+}
