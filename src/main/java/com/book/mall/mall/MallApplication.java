d1c12472bbf794c05ffb5153703690f78b827f34
jingrui <jingrui@36kr.com>
2019-04-23 14:23:48 +0800

    提交打包

--- src/main/java/com/book/mall/mall/MallApplication.java
+++ src/main/java/com/book/mall/mall/MallApplication.java
@@ -3,13 +3,23 @@ package com.book.mall.mall;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @MapperScan(value = "com.book.mall.mall.mapper")
 @SpringBootApplication
-public class MallApplication {
+public class MallApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MallApplication.class, args);
 	}
+
+	//为了打包springboot项目
+	@Override
+	protected SpringApplicationBuilder configure(
+			SpringApplicationBuilder builder) {
+		return builder.sources(this.getClass());
+	}
+
 
 }
