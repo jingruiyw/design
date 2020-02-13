af5a33bcfdfed6440bf64d42efb697b4ff22d85e
jingrui <rui.jing@ninebot.com>
2020-02-13 21:52:15 +0800

    test

---
+++ src/main/java/com/book/mall/thread/learn/ThreadLocalTest.java
@@ -0,0 +1,20 @@
+package com.book.mall.thread.learn;
+
+/**
+ * ClassName: ThreadLocalTest
+ * Description:
+ * date: 2020/1/30 10:24 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+public class ThreadLocalTest {
+
+    private volatile int num;
+
+    public void method() {
+
+        ThreadLocal threadLocal = new ThreadLocal();
+
+    }
+}
