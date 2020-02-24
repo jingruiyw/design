2b8af34c16f42c903b6cf9d76db9860b9229bdfb
jingrui <rui.jing@ninebot.com>
2020-02-24 10:21:56 +0800

    添加spring security 测试

---
+++ src/main/java/com/book/mall/security/LoginController.java
@@ -0,0 +1,24 @@
+package com.book.mall.security;
+
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * ClassName: LoginController
+ * Description:
+ * date: 2020/2/22 5:59 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+@RestController
+@RequestMapping("/user")
+public class LoginController {
+
+    @GetMapping("/login")
+    public Object securityLogin() {
+
+
+
+        return "登录成功";
+    }
+}
