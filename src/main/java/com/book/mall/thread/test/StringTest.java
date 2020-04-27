9eca42966b6671852a22a57884ee248b146b8edb
jingrui <rui.jing@ninebot.com>
2020-04-27 13:02:33 +0800

    添加StringBuilderTest

---
+++ src/main/java/com/book/mall/thread/test/StringTest.java
@@ -0,0 +1,18 @@
+package com.book.mall.thread.test;
+
+/**
+ * ClassName: StringTest
+ * Description:
+ * date: 2020/4/27 12:54 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+public class StringTest {
+
+    public static void main(String[] args) {
+
+        StringBuilder stringBuilder = new StringBuilder("asdfg");
+        System.out.println(stringBuilder.substring(0, stringBuilder.length() - 1));
+    }
+}
