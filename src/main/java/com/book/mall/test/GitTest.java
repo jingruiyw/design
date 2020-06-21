27f8a2aa91e8ad443123ec98b3f9c5115b424178
jingrui <493496950@qq.com>
2020-06-21 20:05:49 +0800

    amend test

---
+++ src/main/java/com/book/mall/test/GitTest.java
@@ -0,0 +1,16 @@
+package com.book.mall.test;
+
+/**
+ * ClassName: GitTest
+ * Description:
+ * date: 2020/6/21 8:04 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("first test");
+    }
+}
