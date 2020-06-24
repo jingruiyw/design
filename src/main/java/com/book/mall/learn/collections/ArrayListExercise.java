38c7164e84e7284812cdb7294ef24a9776b62b47
jingrui <493496950@qq.com>
2020-06-25 01:29:00 +0800

    237.删除链表中的节点

---
+++ src/main/java/com/book/mall/learn/collections/ArrayListExercise.java
@@ -0,0 +1,21 @@
+package com.book.mall.learn.collections;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * ClassName: ArrayListExercise
+ * Description:
+ * date: 2020/6/23 11:50 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+public class ArrayListExercise {
+
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+        System.out.println(2>>1);
+    }
+}
