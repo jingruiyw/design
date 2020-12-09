10ff83df029a8f4f2919454360a6e6267f1adf89
jingrui <493496950@qq.com>
2020-12-09 15:36:09 +0800

    链表：返回倒数第 k 个节点

--- src/main/java/com/book/mall/leetcode/linkedlist/easy/ReverseLinkedList.java
+++ src/main/java/com/book/mall/leetcode/linkedlist/easy/ReverseLinkedList.java
@@ -29,7 +29,24 @@ public class ReverseLinkedList {
         head.next = new ListNode(2);
         head.next.next = new ListNode(3);
 //        System.out.println(reverseList(head));
-        System.out.println(reversListRecursive(head));
+//        System.out.println(reversListRecursive(head));
+        System.out.println(reverseList1(head));
+    }
+
+    public static ListNode reverseList1(ListNode head) {
+        //不为空或者只有一个节点不需要翻转
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = prev; //把当前节点的后一个指针指向前一个
+            prev = curr; //把每个指针都顺延
+            curr = next; //把每个指针都顺延
+        }
+        return prev;
     }
 
 
