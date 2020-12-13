98a3f0c6814981b65bf90c100272fbcd06b2e31a
jingrui <493496950@qq.com>
2020-12-14 00:00:51 +0800

    144. 二叉树的前序遍历:1.递归 2.使用栈

---
+++ src/main/java/com/book/mall/leetcode/tree/BinaryTreePreorderTraversal.java
@@ -0,0 +1,72 @@
+package com.book.mall.leetcode.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * ClassName: BinaryTreePreorderTraversal
+ * Description:
+ * 144. 二叉树的前序遍历
+ * <p>
+ * date: 2020/12/13 11:55 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+public class BinaryTreePreorderTraversal {
+
+
+    /**
+     * 使用栈
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal1(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        if (root == null) {
+            return result;
+        }
+        Stack<TreeNode> stack = new Stack();
+        TreeNode treeNode = root;
+        while (treeNode != null || !stack.isEmpty()) {
+            while (treeNode != null) {
+                result.add(treeNode.val);
+                //把自己放进栈
+                stack.push(treeNode);
+                treeNode = treeNode.left;
+            }
+
+            if (!stack.isEmpty()) {
+                //回溯一次
+                treeNode = stack.pop();
+                //取回溯后的右节点
+                treeNode = treeNode.right;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * 递归
+     *
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        List<Integer> result = new ArrayList<>();
+        preOrder(result, root);
+        return result;
+    }
+
+    //递归
+    public void preOrder(List<Integer> result, TreeNode treeNode) {
+        if (treeNode == null) {
+            return;
+        }
+        result.add(treeNode.val);
+        preOrder(result, treeNode.left);
+        preOrder(result, treeNode.right);
+    }
+}
