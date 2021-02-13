595f134aeac8709258bec506be6266c824d99adf
jingrui <493496950@qq.com>
2021-02-13 18:54:44 +0800

    面试题 08.01. 三步问题

---
+++ src/main/java/com/book/mall/code/dynamicprogramming/ThreeStepsProblemLCCI.java
@@ -0,0 +1,88 @@
+package com.book.mall.code.dynamicprogramming;
+
+/**
+ * ClassName: ThreeStepsProblemLCCI
+ * Description:
+ * 面试题 08.01. 三步问题
+ * 三步问题。有个小孩正在上楼梯，楼梯有n阶台阶，小孩一次可以上1阶、2阶或3阶。实现一种方法，计算小孩有多少种上楼梯的方式。结果可能很大，你需要对结果模1000000007。
+ * <p>
+ * 示例1:
+ * 输入：n = 3
+ * 输出：4
+ * 说明: 有四种走法
+ * <p>
+ * 示例2:
+ * 输入：n = 5
+ * 输出：13
+ * <p>
+ * date: 2021/2/13 6:38 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+public class ThreeStepsProblemLCCI {
+
+
+    public static void main(String[] args) {
+        System.out.println(waysToStep(3));
+    }
+
+    public static int waysToStep(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+        for (int i = 4; i <= n; i++) {
+            dp[i] = (dp[i - 1] + (dp[i - 2] + dp[i - 3]) % 1000000007) % 1000000007;
+        }
+        return dp[n];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
