f725472678204d8ec289f360797b09ad58f98e97
jingrui <493496950@qq.com>
2021-04-18 23:16:33 +0800

    239. 滑动窗口最大值

---
+++ src/main/java/com/book/mall/code/stack/SlidingWindowMaximum.java
@@ -0,0 +1,78 @@
+package com.book.mall.code.stack;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+/**
+ * ClassName: SlidingWindowMaximum
+ * Description:
+ * 239. 滑动窗口最大值
+ * 给你一个整数数组 nums，有一个大小为k的滑动窗口从数组的最左侧移动到数组的最右侧。你只可以看到在滑动窗口内的 k个数字。滑动窗口每次只向右移动一位。
+ *
+ * 返回滑动窗口中的最大值。
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [1,3,-1,-3,5,3,6,7], k = 3
+ * 输出：[3,3,5,5,6,7]
+ * 解释：
+ * 滑动窗口的位置                最大值
+ * ---------------               -----
+ * [1  3  -1] -3  5  3  6  7       3
+ *  1 [3  -1  -3] 5  3  6  7       3
+ *  1  3 [-1  -3  5] 3  6  7       5
+ *  1  3  -1 [-3  5  3] 6  7       5
+ *  1  3  -1  -3 [5  3  6] 7       6
+ *  1  3  -1  -3  5 [3  6  7]      7
+ * 示例 2：
+ *
+ * 输入：nums = [1], k = 1
+ * 输出：[1]
+ * 示例 3：
+ *
+ * 输入：nums = [1,-1], k = 1
+ * 输出：[1,-1]
+ * 示例 4：
+ *
+ * 输入：nums = [9,11], k = 2
+ * 输出：[11]
+ * 示例 5：
+ *
+ * 输入：nums = [4,-2], k = 2
+ * 输出：[4]
+ *
+ * date: 2021/4/18 11:15 下午
+ *
+ * @author: Jingrui
+ * @since JDK 1.8
+ */
+public class SlidingWindowMaximum {
+
+    public int[] maxSlidingWindow(int[] nums, int k) {
+        if (nums == null || nums.length == 0) {
+            return new int[0];
+        }
+        int n = nums.length;
+        Deque<Integer> deque = new LinkedList<>();
+        //把数据放入到队列中
+        for (int i = 0; i < k; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+        }
+        int[] res = new int[n - k + 1];
+        res[0] = nums[deque.peekFirst()];
+        for (int i = k; i < n; ++i) {
+            while (!deque.isEmpty() && nums[i] >= nums[deque.peekLast()]) {
+                deque.pollLast();
+            }
+            deque.offerLast(i);
+            while (deque.peekFirst() <= i - k) {
+                deque.pollFirst();
+            }
+            res[i - k + 1] = nums[deque.peekFirst()];
+        }
+        return res;
+    }
+}
