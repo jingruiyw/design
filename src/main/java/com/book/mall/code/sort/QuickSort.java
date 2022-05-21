0487405e323283d4d31f38fc241320976f905573
jingrui <493496950@qq.com>
2022-05-21 15:37:54 +0800

    排序练习：快排

--- src/main/java/com/book/mall/code/sort/QuickSort.java
+++ src/main/java/com/book/mall/code/sort/QuickSort.java
@@ -17,12 +17,48 @@ public class QuickSort {
 
     public static void main(String[] args) {
         int[] array = {9, 4, 6, 2, 7, 3, 1, 4};
-        int[] array1 = {9, 4, 6, 2, 7, 3, 1, 4,4};
+        int[] array1 = {9, 4, 6, 2, 7, 3, 1, 4, 4};
+        int[] array2 = {9, 4, 6, 2, 7, 3, 1, 4, 4};
         sort(array, 0, 7);
         //[1,2,3,4,4,6,7,9]
         System.out.println(JSON.toJSON(array));
         sort1(array1, 0, 8);
         System.out.println(JSON.toJSON(array1));
+        sort2(array2, 0, 8);
+        System.out.println(JSON.toJSON(array2));
+    }
+
+    private static void sort2(int[] arr, int l, int r) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        if (l >= r) {
+            return;
+        }
+        int left = l;
+        int right = r;
+        int num = arr[left];
+        while (left < right) {
+            while (left < right && arr[right] > num) {
+                right--;
+            }
+            if (left < right) {
+                arr[left] = arr[right];
+            }
+
+            while (left < right && arr[left] <= num) {
+                left++;
+            }
+            if (left < right) {
+                arr[right] = arr[left];
+            }
+            if (left == right) {
+                // 相等时，把基准值补回来
+                arr[left] = num;
+            }
+        }
+        sort2(arr, 0, l - 1);
+        sort2(arr, l + 1, r);
     }
 
     private static void sort1(int[] arr, int l, int r) {
